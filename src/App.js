4e543419fc6606911aaf0c40586d961628f5e23e
dhakalsamundra <seapush02@gmail.com>
2020-12-23 15:18:05 +0200

    add

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Samundra</h1>
+    <div>
       <Form />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -5,27 +5,34 @@ const Register = () => {
   const [user, setUser] = useState({
     name: '',
     email: '',
-    password: '',
-    password2: ''
+    availability: '',
+    flexiable: false
   })
 
-  const { name, email, password, password2 } = user
+  const { name, email, availability, flexiable } = user
 
   const onChange = e => setUser({ ...user, [e.target.name]: e.target.value })
-
+  
   const onSubmit = e => {
     e.preventDefault()
-    if (name === '' || email === '' || password === '') {
-      alert('Please enter all fields')
+    const emailRegx = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    if (name === '') {
+      document.getElementById('name-error').innerHTML = "Mandatory Field!";
     } else if (name.length > 101) {
-        alert('allowed max length is 100')
-    } else {
-      alert('you data has been saved to our database..')
+      document.getElementById('name-error').innerHTML = "Maximun length is 100.";
+    } else if ( availability && !availability.match("^[0-9]+$")){
+      document.getElementById('avai-error').innerHTML = "Must be a number.";
+    } 
+    else if ( email && !email.match(emailRegx)){
+      document.getElementById('email-error').innerHTML = "There must be @ with proper format of email.";
+    }
+    else {
+      window.alert('you data has been saved to our database..')
       setUser({
         name: '',
         email: '',
-        password: '',
-        password2: ''
+        availability: '',
+        flexiable: false
       });
       }
     }
@@ -35,53 +42,50 @@ const Register = () => {
       <h1>
         Account <span className='text-primary'>Register</span>
       </h1>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={onSubmit} id='form'>
         <div className='form-group'>
-          <label htmlFor='name'>Name</label>
+          <label>Name</label>
           <input
             id='name'
             type='text'
             name='name'
             value={name}
             onChange={onChange}
-            required
           />
+          <span id='name-error' className='text-danger'></span>
+
         </div>
+        
         <div className='form-group'>
-          <label htmlFor='email'>Email Address</label>
+          <label>Email Address</label>
           <input
             id='email'
-            type='email'
+            type='text'
             name='email'
             value={email}
             onChange={onChange}
-            required
           />
+          <span id='email-error' className='text-danger'></span>
+
         </div>
+
         <div className='form-group'>
-          <label htmlFor='password'>Password</label>
+          <label>Availability</label>
           <input
-            id='password'
-            type='password'
-            name='password'
-            value={password}
+            id='availability'
+            type='text'
+            name='availability'
+            value={availability}
             onChange={onChange}
-            required
-            minLength='6'
           />
+          <span id='avai-error' className='text-danger'></span>
         </div>
+
         <div className='form-group'>
-          <label htmlFor='password2'>Confirm Password</label>
-          <input
-            id='password2'
-            type='password'
-            name='password2'
-            value={password2}
-            onChange={onChange}
-            required
-            minLength='6'
-          />
+        <label>Flexiable</label><br></br>
+        <input type="checkbox" id='flexiable' name="Flexible" value={flexiable} onChange={onChange} />
         </div>
+
         <input
           type='submit'
           value='Register'
