7603e10a0b2a9b84a8e6dce95523c77e493d8cf8
dhakalsamundra <seapush02@gmail.com>
2020-12-23 15:50:32 +0200

    move to test folder

--- src/test/Form.test.js
+++ src/test/Form.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, fireEvent } from '@testing-library/react'
-import Form from './Form'
+import Form from '../components/Form'
 
 
 test('render content', () => {
